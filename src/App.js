813436e463d7b876321803416d6d1cf3023592a5
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-03-16 17:42:34 +0100

    app to fct component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
+import React, { useState, useEffect } from 'react';
+// import ReactGA from 'react-ga';
+// import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 // import Footer from './Components/Footer';
@@ -10,52 +10,57 @@ import Resume from './Components/Resume';
 // import Testimonials from './Components/Testimonials';
 // import Portfolio from './Components/Portfolio';
 
-class App extends Component {
+function App () {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
+  const [ resumeData, setresumeData ] = useState({})
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
 
+    // ReactGA.initialize('UA-110570651-1');
+    // ReactGA.pageview(window.location.pathname);
+
+  // getResumeData(){
+  //   $.ajax({
+  //     url:'/resumeData.json',
+  //     dataType:'json',
+  //     cache: false,
+  //     success: function(data){
+  //       this.setState({resumeData: data});
+  //     }.bind(this),
+  //     error: function(xhr, status, err){
+  //       console.log(err);
+  //       alert(err);
+  //     }
+  //   });
+  // }
+    // ***************************************
+  async function getResumeData() {
+    const response = await fetch('/resumeData.json');
+    const resumeData = await response.json();
+    console.log(resumeData);
   }
+  
+  getResumeData();
+  
+  useEffect (() => {
+    setresumeData(resumeData)
+  },[])
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
+  // componentDidMount(){
+  //   this.getResumeData();
+  // }
 
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        {/* <Portfolio data={this.state.resumeData.portfolio}/> */}
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
-        {/* <Contact data={this.state.resumeData.main}/> */}
-        {/* <Footer data={this.state.resumeData.main}/> */}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header data={resumeData.main}/>
+      <About data={resumeData.main}/>
+      <Resume data={resumeData.resume}/>
+      {/* <Portfolio data={resumeData.portfolio}/> */}
+      {/* <Testimonials data={resumeData.testimonials}/> */}
+      {/* <Contact data={resumeData.main}/> */}
+      {/* <Footer data={resumeData.main}/> */}
+    </div>
+  );
+  
 }
 
 export default App;
