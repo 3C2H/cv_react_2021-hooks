862c75641a9e76da333a74cd80d88f49bdf5f4a8
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-04-27 15:26:48 +0200

    display context default: display

--- src/displayContext.js
+++ src/displayContext.js
@@ -4,7 +4,7 @@ const DisplayContext = React.createContext()
 
 
 function DisplayContextProvider(props) {
-  const [displayStatus, setDisplayStatus] = useState('none')
+  const [displayStatus, setDisplayStatus] = useState('flex')
 
   function toggleDisplay() {
     displayStatus === 'none' ? setDisplayStatus('flex') : setDisplayStatus('none')
@@ -21,27 +21,3 @@ function DisplayContextProvider(props) {
 }
 
 export { DisplayContextProvider, DisplayContext }
-
-// import React, { useState } from 'react'
-
-// const DisplayContext = React.createContext()
-
-
-// function DisplayContextProvider(props) {
-//   const [displayStatus, setDisplayStatus] = useState('off')
-
-//   function toggleDisplay() {
-//     displayStatus === 'off' ? setDisplayStatus('on') : setDisplayStatus('off')
-//   }
-
-//   return(
-//     <DisplayContext.Provider
-//       value={{ displayStatus: displayStatus, toggleDisplay: toggleDisplay }}
-//     >
-//       { props.children }
-//     </DisplayContext.Provider>
-//   )
-
-// }
-
-// export { DisplayContextProvider, DisplayContext }
